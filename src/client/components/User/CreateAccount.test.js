4475d049fd3bc1d3e184f67468ba49bb04a453e9
Robisson Oliveira <robisson@robissonoliveira.com.br>
2017-10-08 21:02:04 -0300

    Adding test to create account user

---
+++ src/client/components/User/CreateAccount.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Provider} from 'react-redux';
+import CreateAccount from './CreateAccount';
+
+// setup file
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import {mount, shallow} from 'enzyme'
+configure({adapter: new Adapter()});
+import configureStore from 'redux-mock-store'
+
+describe('Create Account User', () => {
+
+    let createAccount;
+
+    const testValues = {
+        state: {
+            success: false,
+            showPreloader: false
+        },
+        onCreateAccount: jest.fn()
+    };
+
+    beforeEach(() => {
+        createAccount = shallow(<CreateAccount {...testValues}/>);
+    });
+
+    it('should have 5 inputs', () => {
+        expect(createAccount.find('input').length).toEqual(5);
+    });
+
+    it('should have 1 button for submit', () => {
+        expect(createAccount.find('button').length).toEqual(1);
+    });
+
+    it('Should not submit, missed fields', () => {
+        createAccount
+            .find('button')
+            .simulate('click');
+        expect(testValues.onCreateAccount).toHaveBeenCalledTimes(0);
+    });
+
+});
