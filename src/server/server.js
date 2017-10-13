6ce4300720822f014e216a81bd7778e0ae9e167a
Robisson Oliveira <robisson@robissonoliveira.com.br>
2017-10-12 21:22:34 -0300

    Adding routes for the api create account

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,17 @@ import render from './ServerRender'
 const app = Express()
 const port = 3000
 
+const api = Express.Router()
+
+api.post('/createAccount', (req, res) => {
+    console.log(req);
+    return res
+        .status(200)
+        .end()
+})
+
+app.use('/api/v1', api);
+
 //Serve static files
 app.use('/dist', Express.static(__dirname + '/../../public/dist'));
 app.use('/assets', Express.static(__dirname + '/../../public/assets'));
