13e93b790403e99e6b5712afca4f3a91fa47860a
Robisson Oliveira <robisson@robissonoliveira.com.br>
2017-10-21 21:55:33 -0200

    Change structure from apirouter  to controller

--- src/server/apiRoutes.js
+++ src/server/apiRoutes.js
@@ -1,45 +1,8 @@
 import Express from 'express';
-import passport from 'passport';
-import User from './models/user';
+import CreateAccount from './controllers/CreateAccount'
 
 const api = Express.Router()
 
-api.post('/createAccount', (req, res, next) => {
-
-    let name = req.body.name;
-    let lastname = req.body.lastname;
-    let email = req.body.email;
-    let password = req.body.password;
-
-    // verify if user already exists
-    User.findOne({
-        email: email
-    }, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-        if (user) {
-            return res
-                .status(200)
-                .json({success: false, message: "User already exists"});
-        }
-
-        let newUser = new User({name, lastname, email, password});
-
-        newUser
-            .save(next)
-            .then(user => {
-                return res
-                .status(200)
-                .json({success: true,message:"User account created with successfull!", user});
-            })
-            .catch(err => {
-                return res
-                .status(500)
-                .json({success: false, message: err});
-            });
-
-    });
-});
+api.post('/createAccount', CreateAccount);
 
 export default api;
---
+++ src/server/controllers/CreateAccount.js
@@ -0,0 +1,42 @@
+import User from '../models/user';
+import passport from 'passport';
+
+const CreateAccount = (req, res, next) => {
+
+    let name = req.body.name;
+    let lastname = req.body.lastname;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    // verify if user already exists
+    User.findOne({
+        email: email
+    }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            return res
+                .status(200)
+                .json({success: false, message: "User already exists"});
+        }
+
+        let newUser = new User({name, lastname, email, password});
+
+        newUser
+            .save(next)
+            .then(user => {
+                return res
+                    .status(200)
+                    .json({success: true, message: "User account created with successfull!"});
+            })
+            .catch(err => {
+                return res
+                    .status(500)
+                    .json({success: false, message: err});
+            });
+
+    });
+};
+
+export default CreateAccount;
