1dbbea87b7b56576a850ad90d7bc1ac68844a9eb
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-03-21 20:25:10 -0300

    finish request when token is not provided

--- src/server/middlewares/requireAuth.js
+++ src/server/middlewares/requireAuth.js
@@ -4,11 +4,11 @@ export function requireAuth(req, res, next) {
   //for development token is not required
   if (!__DEV__) {
     if (req.authFailed) {
-      return res.status(403).json({ message: "Failed to authenticate token" });
+      return res.status(403).json({ message: "Failed to authenticate token" }).end();
     }
 
     if (req.noTokenProvided) {
-      return res.status(403).json({ message: "No token provided" });
+      return res.status(403).json({ message: "No token provided" }).end();
     }
   }
 
