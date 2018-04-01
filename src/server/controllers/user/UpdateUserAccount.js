586b339d13ba713652198131242890a1cb425af1
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-04-01 19:50:46 -0300

    Adding controller to update user account

--- src/server/controllers/user/UpdateUserAccount.js
+++ src/server/controllers/user/UpdateUserAccount.js
@@ -1,21 +1,23 @@
 import User from "../../models/user";
 
 const UpdateUserAccount = (req, res) => {
-  let { _id, name, lastname, phone, email, firstAccess } = JSON.parse(
-    req.body.user
-  );
+  let { name, lastname, phone, email, firstAccess } = JSON.parse(req.body.user);
+
+  let { image } = req.body;
+  let _id = req.params.userid;
 
   User.findById(_id, function(err, user) {
-    if (err) handleError(res);
+    if (err) handleError(res, err);
 
     user.name = name;
     user.lastname = lastname;
     user.email = email;
     user.phone = phone;
     user.firstAccess = firstAccess;
+    user.image = image;
 
     user.save(function(err) {
-      if (err) handleError(res);
+      if (err) handleError(res, err);
 
       res
         .status(200)
@@ -27,10 +29,10 @@ const UpdateUserAccount = (req, res) => {
   });
 };
 
-function handleError(res) {
+function handleError(res, err) {
   return res
     .status(500)
-    .json({ success: false })
+    .json({ success: false, error: err })
     .end();
 }
 
