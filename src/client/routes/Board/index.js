5a35738b731fe8e184f0bea70eb280c7cc85b3a7
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-07-04 20:11:46 -0300

    Split components in the Navbar

--- src/client/routes/Board/index.js
+++ src/client/routes/Board/index.js
@@ -1,21 +1,24 @@
 // @flow
 import * as React from "react";
-import {withStyles} from '@material-ui/core/styles';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Navbar from '../Navbar'
-import Column from '../Board/Column'
+import { withStyles } from "@material-ui/core/styles";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Navbar from "../Navbar/index";
+import Column from "../Board/Column";
+import MenuLeft from '../Navbar/MenuLeft';
+import MenuRight from '../Navbar/MenuRight';
+import columns from './fakeData';
 
 const theme = createMuiTheme();
 
-const styles : Object = {
+const styles: Object = {
   root: {
     flexGrow: 1
   },
   gridContainer: {
-    overflowX:'auto',
-    marginTop:68,
-    height:700
+    overflowX: "auto",
+    marginTop: 68,
+    height: 700
   }
 };
 
@@ -23,135 +26,33 @@ type Props = {
   classes: Object
 };
 
-function LayoutApp(props : Props) {
-  const {classes} = props;
+function LayoutApp(props: Props) {
+  const { classes } = props;
 
-  let columns = [
-    {
-      id:4,
-      title: "A fazer",
-      hColumns:[],
-      vColumns:[],
-      cards:[
-        {
-          id: 31,
-          title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-            "o quadro kanban"
-        }
-      ]
-    },
-    {
-      id:1,
-      title: "Executando",
-      hColumns:[
-        {
-          id:2,
-          title: "Chamados",
-          hColumns:[],
-          vColumns:[
-            {
-              id:4,
-              title: "Parado",
-              hColumns:[],
-              vColumns:[],
-              cards:[]
-            },
-            {
-              id:5,
-              title: "Executando",
-              hColumns:[],
-              vColumns:[],
-              cards:[
-                {
-                  id: 35,
-                  title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-                    "o quadro kanban"
-                }
-              ]
-            }
-          ],
-          cards:[]
-        },
-        {
-          id:3,
-          title: "Projetos",
-          hColumns:[],
-          vColumns:[
-            {
-              id:41,
-              title: "Parado",
-              hColumns:[],
-              vColumns:[],
-              cards:[
-                {
-                  id: 36,
-                  title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-                    "o quadro kanban"
-                }
-              ]
-            },
-            {
-              id:51,
-              title: "Executando",
-              hColumns:[],
-              vColumns:[],
-              cards:[]
-            }
-          ],
-          cards:[]
-        }
-      ],
-      vColumns:[],
-      cards:[]
-    },
-    {
-      id:9,
-      title: "Em teste",
-      hColumns:[],
-      vColumns:[],
-      cards:[
-        {
-          id: 32,
-          title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-            "o quadro kanban"
-        }
-      ]
-    },
-    {
-      id:10,
-      title: "Concluído",
-      hColumns:[],
-      vColumns:[],
-      cards:[
-        {
-          id: 33,
-          title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-            "o quadro kanban"
-        },
-        {
-          id: 34,
-          title: "Criar layouts da tela principal do quadro. Este é um exemplo de tarefa ou card d" +
-            "o quadro kanban"
-        }
-      ]
-    }
-  ];
+
 
   return (
     <MuiThemeProvider theme={theme}>
-      <Navbar/>
+      <Navbar>
+        <MenuLeft />
+        <MenuRight />
+      </Navbar>
       <Grid
         container
         justify="flex-start"
         wrap="nowrap"
         spacing={0}
-        className={classes.gridContainer}>
-
-        {columns.map(column => (<Column key={column.id}
-                                        vColumns={column.vColumns}
-                                        hColumns={column.hColumns}
-                                        title={column.title}
-                                        cards={column.cards}/>))}
+        className={classes.gridContainer}
+      >
+        {columns.map(column => (
+          <Column
+            key={column.id}
+            vColumns={column.vColumns}
+            hColumns={column.hColumns}
+            title={column.title}
+            cards={column.cards}
+          />
+        ))}
       </Grid>
     </MuiThemeProvider>
   );
