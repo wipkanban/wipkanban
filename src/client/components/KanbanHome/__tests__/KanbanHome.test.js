7c5f38a2cedf809895cd80750722aa9757dfe342
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-10-12 19:05:33 -0300

    Add tests to KanHome, Navbar and MenuLeft components

---
+++ src/client/components/KanbanHome/__tests__/KanbanHome.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import KanbanHome from "../index";
+import { createShallow } from "@material-ui/core/test-utils";
+import Grid from "@material-ui/core/Grid";
+import BoardList from "../../Board/BoardList";
+import TeamList from "../../Team/TeamList";
+
+// setup file
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("KanbanHome component", () => {
+  let shallow;
+
+  beforeEach(() => {
+    shallow = createShallow();
+  });
+
+  it("should render 1 containing 1 TeamList and BoardListComponent", () => {
+    let wrapper = shallow(<KanbanHome />);
+    expect(wrapper.type()).toEqual(Grid);
+    expect(wrapper.find(BoardList).type()).toEqual(BoardList);
+    expect(wrapper.find(TeamList).type()).toEqual(TeamList);
+  });
+});
---
+++ src/client/components/Navbar/__tests__/Navbar.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../index";
+import { createShallow } from "@material-ui/core/test-utils";
+import AppBar from '@material-ui/core/AppBar';
+
+// setup file
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("Navbar Component", () => {
+  let shallow;
+
+  beforeEach(() => {
+    shallow = createShallow({dive:true});
+  });
+
+  it("should render a AppBar Component", () => {
+    let wrapper = shallow(<Navbar />);
+    expect(wrapper.find(AppBar).type()).toEqual(AppBar);
+  });
+
+  it("should render its children", () => {
+    let wrapper = shallow(<Navbar><div>Hello</div></Navbar>);
+    expect(wrapper.find('div').type()).toEqual('div');
+    expect(wrapper.find('div').at(0).text()).toEqual('Hello');
+  });
+
+});
