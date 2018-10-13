fd2367a8cc3c0667eeb40223e1a34d2b35ac6cad
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-10-13 11:27:11 -0300

    Add snapshots tests to <TeamList />

--- src/client/components/Team/TeamList.test.js
+++ src/client/components/Team/TeamList.test.js
@@ -3,6 +3,7 @@ import TeamList from "./TeamList";
 import { createShallow } from "@material-ui/core/test-utils";
 import Typography from "@material-ui/core/Typography";
 import Tooltip from "@material-ui/core/Tooltip";
+import toJson from "enzyme-to-json";
 
 // setup file
 import { configure } from "enzyme";
@@ -10,21 +11,24 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-describe("TeamList Component", () => {
+describe("<TeamList /> Component", () => {
   let shallow;
+  let wrapper;
 
   beforeEach(() => {
     shallow = createShallow();
+    wrapper = shallow(<TeamList />);
+  });
+
+  it("should render an component correctly", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it("should render a div", () => {
-    let wrapper = shallow(<TeamList />);
     expect(wrapper.type()).toEqual("div");
   });
 
   it("should render 3 Typogragh and 1 Tooltip", () => {
-    let wrapper = shallow(<TeamList />);
     expect(wrapper.find(Typography).length).toEqual(2);
-    expect(wrapper.find(Tooltip).length).toEqual(1);
-  });
+    expect(wrapper.find(Tooltip).length).toEqual(1);  });
 });
