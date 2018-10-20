6edcfbcd38cbfc6d984040caafecfa7ab7107543
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-10-20 10:38:33 -0300

    Refactor tests to BoardLista and KanbanHome component

--- src/client/components/Board/BoardList.test.js
+++ src/client/components/Board/BoardList.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import BoardList from "./BoardList";
 import { createShallow } from "@material-ui/core/test-utils";
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { boards } from "../../fakeData";
+import toJson from "enzyme-to-json";
 
 // setup file
 import { configure } from "enzyme";
@@ -18,13 +20,15 @@ describe("BoardList Component", () => {
   });
 
   it("should render a div", () => {
-    let wrapper = shallow(<BoardList />);
+    let wrapper = shallow(<BoardList boards={boards} />);
     expect(wrapper.type()).toEqual("div");
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it("should render 1 Typogragh and 2 Grid", () => {
-    let wrapper = shallow(<BoardList />);
+    let wrapper = shallow(<BoardList boards={boards} />);
     expect(wrapper.find(Typography).length).toEqual(1);
     expect(wrapper.find(Grid).length).toEqual(2);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
