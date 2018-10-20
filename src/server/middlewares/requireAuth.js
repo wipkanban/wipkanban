8d4f32932b44320c1760c0f841a33b39b2f0d1cb
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-10-20 11:55:39 -0300

    Add rquired token condition to all request in the api

--- src/server/middlewares/requireAuth.js
+++ src/server/middlewares/requireAuth.js
@@ -1,15 +1,16 @@
-import __DEV__ from "../../isDev";
-
 export function requireAuth(req, res, next) {
-  //for development token is not required
-  if (!__DEV__) {
-    if (req.authFailed) {
-      return res.status(403).json({ message: "Failed to authenticate token" }).end();
-    }
+  if (req.authFailed) {
+    return res
+      .status(403)
+      .json({ message: "Failed to authenticate token" })
+      .end();
+  }
 
-    if (req.noTokenProvided) {
-      return res.status(403).json({ message: "No token provided" }).end();
-    }
+  if (req.noTokenProvided) {
+    return res
+      .status(403)
+      .json({ message: "No token provided" })
+      .end();
   }
 
   next();
