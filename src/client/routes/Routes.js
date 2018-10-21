8ed5f63398e06a9b84d948b50f92cb4965fcf579
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-10-20 22:34:16 -0300

    Remove route unused

--- src/client/routes/Routes.js
+++ src/client/routes/Routes.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginContainer from "../components/Login/LoginContainer";
 import SignupContainer from "../components/Signup/SignupContainer";
 import AccountCreated from "../components/Signup/AccountCreated";
-import HomeContainer from "../components/Home";
+import HomeContainer from "../components/Home/HomeContainer";
 import requireAuth from "../components/requireAuth";
 import BoardContainer from "../components/Board/BoardContainer";
 import Signup from "../components/Signup";
