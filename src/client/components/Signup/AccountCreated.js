0744e31d9bada783d84ff8c3dfa179e2be1e9127
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-11-10 12:17:29 -0200

    Refacotoring <AccountCreated /> and its tests

--- src/client/components/Signup/AccountCreated.js
+++ src/client/components/Signup/AccountCreated.js
@@ -1,20 +1,121 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
+import green from "@material-ui/core/colors/green";
 
-const AccountCreated = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    height: "100vh",
+    display: "flex",
+    alignItems: "center"
+  },
+  backgroundPrimary: {
+    backgroundColor: theme.palette.primary.main
+  },
+  text: {
+    width: "100%"
+  },
+  link: {
+    textDecoration: "none",
+    color: theme.palette.secondary.main,
+    "&:hover": {
+      textDecoration: "underline"
+    }
+  },
+  textSuccessfull: {
+    color: green[500]
+  }
+});
+
+type Props = {
+  classes: Object
+};
+
+const AccountCreated = ({ classes }: Props) => {
   return (
-    <div className="text-center">
-      <h1>
-        Your account has been successfully created.<br /> Click the link below
-        to login
-      </h1>
-      <p>
-        <Link to="/login" className="btn btn-success">
-          Login
-        </Link>
-      </p>
-    </div>
+    <Grid container className={classes.root}>
+      <Grid item xs={12} className={classes.demo}>
+        <Grid
+          container
+          className={classes.demo}
+          direction="row"
+          justify="flex-start"
+        >
+          <Grid item xs={12} sm={12} md={7} lg={8} xl={8}>
+            <Paper
+              elevation={0}
+              className={[classes.paper, classes.backgroundPrimary].join(" ")}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 10
+              }}
+            >
+              <div>
+                <br />
+                <Typography
+                  style={{
+                    color: "white"
+                  }}
+                  variant="h4"
+                  gutterBottom
+                >
+                  Welcome to the best open source plataform for lean management
+                </Typography>
+                <Typography
+                  style={{
+                    color: "white"
+                  }}
+                  variant="h5"
+                  gutterBottom
+                >
+                  Align strategy and execution with <b>OKRs</b> and{" "}
+                  <b>Kanban</b> for you, your team and your company.
+                </Typography>
+              </div>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={5} lg={4} xl={4}>
+            <Paper
+              elevation={0}
+              className={classes.paper}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 10
+              }}
+            >
+              <div style={{ margin: "0 auto" }}>
+                <Typography
+                  className={classes.textSuccessfull}
+                  align="center"
+                  variant="h4"
+                  gutterBottom
+                >
+                  Your account has been successfully created
+                </Typography>
+                <br />
+                <Typography align="center">
+                  Click at link in the bellow to do login
+                </Typography>
+                <br />
+                <Typography variant="h5" align="center" gutterBottom>
+                  <Link className={classes.link} to="/login">
+                    Login
+                  </Link>
+                </Typography>
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
-export default AccountCreated;
+export default withStyles(styles)(AccountCreated);
--- src/client/components/Signup/AccountCreated.test.js
+++ src/client/components/Signup/AccountCreated.test.js
@@ -4,23 +4,25 @@ import { createShallow } from "@material-ui/core/test-utils";
 import { Link } from "react-router-dom";
 import toJson from "enzyme-to-json";
 import "../../setupTest";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 describe("<AccountCreated /> Component", () => {
   let shallow;
   let wrapper;
 
   beforeEach(() => {
-    shallow = createShallow();
+    shallow = createShallow({ dive: true });
     wrapper = shallow(<AccountCreated />);
   });
 
-  it("should render component correctly", () => {
-    expect(wrapper.type()).toEqual("div");
+  it("should render correctly ", () => {
+    expect(wrapper.find(Grid).length).toEqual(5);
+    expect(wrapper.find(Typography).length).toEqual(5);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it("should have a h1 and a Link to /login", () => {
-    expect(wrapper.find("h1").length).toEqual(1);
+  it("should have a Link to /login", () => {
     expect(wrapper.find(Link).props().to).toEqual("/login");
   });
 });
