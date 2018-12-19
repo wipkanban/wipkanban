4d73e56cc06cbd2826c94346c1d81a58867ef4b0
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-12-19 21:40:31 -0200

    Fix import in <FormLogin />

--- src/client/components/Login/FormLogin.test.js
+++ src/client/components/Login/FormLogin.test.js
@@ -3,7 +3,6 @@ import FormLogin from "./index";
 import { createShallow } from "@material-ui/core/test-utils";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import IconButton from "@material-ui/core/IconButton";
 import { Redirect } from "react-router-dom";
 import "../../setupTest";
 import toJson from "enzyme-to-json";
