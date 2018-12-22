5210e09956e70f6058114196e1b07757b3f45bcf
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-12-22 12:17:47 -0200

    Add link to home and remove unusued code

--- src/client/components/Navbar/index.js
+++ src/client/components/Navbar/index.js
@@ -1,11 +1,9 @@
 // @flow
 import * as React from "react";
-import {withStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
   root: {
@@ -20,27 +18,29 @@ const styles = {
   menuButton: {
     marginLeft: -12,
     marginRight: 20
+  },
+  textWhite: {
+    color: "white",
+    textDecoration: "none"
   }
 };
 
 type Props = {
   classes: Object,
-  children?:React.Node
+  children?: React.Node
 };
 
-function Navbar(props : Props) {
-  const {classes,children} = props;
+function Navbar(props: Props) {
+  const { classes, children } = props;
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-          <MenuIcon/>
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          WIPLean
-        </Typography>
+        <a href="/" className={classes.textWhite}>
+          <Typography variant="h6" color="inherit">
+            WIPLean
+          </Typography>
+        </a>
         {children}
-
       </Toolbar>
     </AppBar>
   );
