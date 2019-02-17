1f8bf8b313cc5cda8d98e6b7c6b8c2ec9775d439
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 11:44:23 -0300

    Refactory tests in <Home /> to Typescript

--- src/client/components/Home/Home.test.tsx
+++ src/client/components/Home/Home.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import Home from "./index";
 import Navbar from "../Navbar";
 import { createShallow } from "@material-ui/core/test-utils";
@@ -6,12 +6,10 @@ import TabsHome from "./TabsHome";
 import MenuRight from "../Navbar/MenuRight";
 import Search from "../Navbar/Search";
 import UserAccountSettings from "../Navbar/Buttons/UserAccountSettingsContainer";
-import toJson from "enzyme-to-json";
-import "../../setupTest";
 
 describe("<Home /> component", () => {
-  let shallow;
-  let user;
+  let shallow: any;
+  let user: { firstAccess: boolean };
 
   beforeEach(() => {
     shallow = createShallow();
@@ -25,7 +23,7 @@ describe("<Home /> component", () => {
     expect(wrapper.type()).toEqual("div");
     expect(wrapper.find(Navbar).length).toEqual(1);
     expect(wrapper.find(Navbar).length).toEqual(1);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render a RightMenu with two icons component", () => {
@@ -45,14 +43,14 @@ describe("<Home /> component", () => {
         .type()
     ).toEqual(UserAccountSettings);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render a TabHome component", () => {
     let wrapper = shallow(<Home user={user} />);
 
     expect(wrapper.find(TabsHome).type()).toEqual(TabsHome);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render a WelcomePage component when user firstAccess is true", () => {
@@ -60,6 +58,6 @@ describe("<Home /> component", () => {
     let wrapper = shallow(<Home user={user} />);
 
     expect(wrapper.name()).toEqual("Connect(WithStyles(WelcomePage))");
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
