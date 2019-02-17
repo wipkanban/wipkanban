4298e3d999bf0c5dfe818b10b234e40fd9266499
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 11:08:00 -0300

    Refactory tests in <Card /> to Typescript

--- src/client/components/Board/Card.test.tsx
+++ src/client/components/Board/Card.test.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Card from "./Card";
-import CardMaterialUi from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import CardMaterialUi from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import { createShallow } from "@material-ui/core/test-utils";
-import toJson from "enzyme-to-json";
-import "../../setupTest"
+import { ShallowWrapper } from "enzyme";
 
 describe("<Card /> Component", () => {
-  let shallow;
-  let wrapper;
+  let shallow: any;
+  let wrapper: ShallowWrapper;
 
   beforeEach(() => {
     shallow = createShallow({ dive: true });
@@ -18,20 +17,20 @@ describe("<Card /> Component", () => {
     wrapper = shallow(<Card title="card title" />);
 
     expect(wrapper.type()).toEqual("div");
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should have a Card component", () => {
     wrapper = shallow(<Card title="card title" />);
 
     expect(wrapper.find(CardMaterialUi).type()).toEqual(CardMaterialUi);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should have a CardContent component", () => {
     wrapper = shallow(<Card title="card title" />);
 
     expect(wrapper.find(CardContent).type()).toEqual(CardContent);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
