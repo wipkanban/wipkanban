bd5c6c15d8245cedc4950d293139c842dcc713dd
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 10:53:20 -0300

    Refactory tests in >BoardList /> to Typescript

--- src/client/components/Board/BoardList.test.tsx
+++ src/client/components/Board/BoardList.test.tsx
@@ -4,11 +4,9 @@ import { createShallow } from "@material-ui/core/test-utils";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { boards } from "../../fakeData";
-import toJson from "enzyme-to-json";
-import "../../setupTest"
 
 describe("BoardList Component", () => {
-  let shallow;
+  let shallow: any;
 
   beforeEach(() => {
     shallow = createShallow();
@@ -17,13 +15,13 @@ describe("BoardList Component", () => {
   it("should render a div", () => {
     let wrapper = shallow(<BoardList boards={boards} />);
     expect(wrapper.type()).toEqual("div");
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render 1 Typogragh and 2 Grid", () => {
     let wrapper = shallow(<BoardList boards={boards} />);
     expect(wrapper.find(Typography).length).toEqual(1);
     expect(wrapper.find(Grid).length).toEqual(2);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
