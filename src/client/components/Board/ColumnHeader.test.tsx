4f3d9a9b4f280d1a9ad0c808b1cc8f9be210e454
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 11:20:14 -0300

    Refactory tests in <ColumnHeader /> to Typescript

--- src/client/components/Board/ColumnHeader.test.tsx
+++ src/client/components/Board/ColumnHeader.test.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import ColumnHeader from "./ColumnHeader";
 import { createShallow } from "@material-ui/core/test-utils";
-import toJson from "enzyme-to-json";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import "../../setupTest"
+import { ShallowWrapper } from "enzyme";
 
 describe("<ColumnHeader /> Component", () => {
-  let shallow;
-  let wrapper;
+  let shallow: any;
+  let wrapper: ShallowWrapper;
 
   beforeEach(() => {
     shallow = createShallow({ dive: true });
-    wrapper = shallow(<ColumnHeader />);
+    wrapper = shallow(<ColumnHeader title="title" />);
   });
 
   it("should render correctly", () => {
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render 3 items components", () => {
     expect(wrapper.find(ListItemText).type()).toEqual(ListItemText);
     expect(wrapper.find(ListItemIcon).type()).toEqual(ListItemIcon);
-    expect(wrapper.find(ListItemSecondaryAction).type()).toEqual(ListItemSecondaryAction);
+    expect(wrapper.find(ListItemSecondaryAction).type()).toEqual(
+      ListItemSecondaryAction
+    );
   });
 });
