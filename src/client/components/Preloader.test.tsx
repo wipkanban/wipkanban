bff089c2a5cdffb7a1fbdca8e9ba513341935ef7
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 12:38:56 -0300

    Refactory tests in <Preloader /> to Typescript

--- src/client/components/Preloader.test.tsx
+++ src/client/components/Preloader.test.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import toJson from "enzyme-to-json";
+import * as React from "react";
 import { createShallow } from "@material-ui/core/test-utils";
-import "../setupTest";
 import Preloader from "./Preloader";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { ShallowWrapper } from "enzyme";
 
 describe("<Preloader /> Component", () => {
-  let wrapper;
-  let shallow;
+  let wrapper: ShallowWrapper;
+  let shallow: any;
 
   beforeEach(() => {
     shallow = createShallow({ dive: true });
@@ -16,12 +15,12 @@ describe("<Preloader /> Component", () => {
   it("should return empty content when showPrealoder is not defined", () => {
     wrapper = shallow(<Preloader />);
     expect(wrapper.type()).toBeNull;
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should to render <CircularProgress /> when showPreloader is true", () => {
     wrapper = shallow(<Preloader showPreloader={true} />);
     expect(wrapper.find(CircularProgress).type()).toEqual(CircularProgress);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
