348997af6fd591e8f48bccd86dc65d813d2b913a
Robisson Oliveira <robisson@robissonoliveira.com.br>
2019-02-17 12:17:59 -0300

    Refacotyr tests in <MenuRight /> to Typescript

--- src/client/components/Navbar/MenuRight.test.tsx
+++ src/client/components/Navbar/MenuRight.test.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import MenuRight from "./MenuRight";
 import { createShallow } from "@material-ui/core/test-utils";
-import toJson from "enzyme-to-json";
-
-import "../../setupTest"
+import { ShallowWrapper } from "enzyme";
 
 describe("MenuRight Component", () => {
-  let shallow;
+  let shallow: any;
 
   beforeEach(() => {
     shallow = createShallow({ dive: true });
@@ -15,16 +13,16 @@ describe("MenuRight Component", () => {
   it("should render a Tooltip", () => {
     let wrapper = shallow(<MenuRight />);
     expect(wrapper.type()).toEqual("div");
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should render its children", () => {
-    let wrapper = shallow(
+    let wrapper: ShallowWrapper = shallow(
       <MenuRight>
         <div>Hello</div>
       </MenuRight>
     );
     expect(wrapper.childAt(0).text()).toEqual("Hello");
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
